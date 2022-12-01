7988c6a8b2d97d589b5f7a7b9c68d4c2f5bd6fc0
Brian <bmdonald1998@gmail.com>
2022-12-01 17:17:53 -0600

    day 1

---
+++ d1/main.nim
@@ -0,0 +1,37 @@
+import std/algorithm
+import std/parseutils
+import std/strformat
+
+
+let f = open("./input.txt")
+
+var line: string
+var currentTotal = 0
+var totals = newSeq[int]()
+
+while not endOfFile(f):
+    line = f.readLine()
+    
+    if line == "":
+        totals.add(currentTotal)
+        currentTotal = 0
+        continue
+    
+    var amount: int
+    discard parseInt(line, amount)
+    
+    
+    currentTotal = currentTotal + amount
+
+totals.add(currentTotal)
+echo "Totals"
+
+totals.sort()
+
+echo "The man with the most has"
+echo totals[totals.len - 1]
+
+
+echo "The others have"
+echo fmt"{totals[totals.len - 1]} {totals[totals.len - 2]} {totals[totals.len - 3]}"
+echo totals[totals.len - 1] + totals[totals.len - 2] + totals[totals.len - 3]
